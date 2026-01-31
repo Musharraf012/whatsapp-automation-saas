f3fbb6cd219df482b8eba3e887a968188b6d807a
Musharraf012 <157002498+Musharraf012@users.noreply.github.com>
2026-01-31 15:15:28 +0530

    change to run on 5 pm everyday

--- src/services/cron.service.js
+++ src/services/cron.service.js
@@ -9,7 +9,7 @@ import { LANGUAGE_SELECTION_TEMPLATE } from "../constants.js";
  * Runs at 9 AM on the 5th of every month
  * Starts the monthly guided service flow
  */
-cron.schedule("* * * * *", async () => {
+cron.schedule("* 17 * * *", async () => {
     console.log("⏰ Monthly WhatsApp Service Flow Started");
 
     const today = new Date();
